b03ecccfeccb2069501ace03a0073d30cb025ca1
Hernan Aguilera Abril <haguil0122@gmail.com>
2023-11-06 13:41:34 +0100

    filtering paths and ploting losses

---
+++ plot.py
@@ -0,0 +1,59 @@
+import os 
+import numpy as np 
+import json
+import matplotlib.pyplot as plt
+
+# plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+
+history_path = "./history"
+paths = [path for path in os.listdir(history_path)]
+kvasir_paths = [path for path in paths if "_kvasir" in path]
+bagls_paths = [path for path in paths if "_kvasir" not in path]
+
+def filter_paths(paths, current_num_nests, current_num_filters, current_operation):
+    filtered_paths = [path for path in paths if f"history_{current_num_nests}{current_num_filters}{current_operation}" in path]
+    return filtered_paths
+
+# Use the function to filter the paths
+current_num_nests = 2
+current_num_filters = 64
+current_operation = 'add'
+filtered_paths = filter_paths(paths, current_num_nests, current_num_filters, current_operation)
+
+# Create a figure with two subplots: one for the loss and one for the IoU
+fig, axes = plt.subplots(2, figsize=(12, 12))
+
+for path in bagls_paths:
+    with open(os.path.join(history_path, path), 'r') as f:
+        history = json.load(f)
+
+    # Plot the loss in the first subplot
+    axes[0].plot(history['loss'], label='Training Loss for ' + path, color='blue')
+    axes[0].plot(history['val_loss'], label='Validation Loss for ' + path, color='red')
+
+    # Plot the IoU in the second subplot
+    axes[1].plot(history['iou_score'], label='Training IoU for ' + path, color='blue')
+    axes[1].plot(history['val_iou_score'], label='Validation IoU for ' + path, color='red')
+
+# Set the title, x-axis label, and y-axis label of the loss subplot
+axes[0].set_title('Loss for All History Files', fontsize=16)
+axes[0].set_xlabel('Epochs', fontsize=14)
+axes[0].set_ylabel('Loss', fontsize=14)
+
+# Set the title, x-axis label, and y-axis label of the IoU subplot
+axes[1].set_title('IoU for All History Files', fontsize=16)
+axes[1].set_xlabel('Epochs', fontsize=14)
+axes[1].set_ylabel('IoU', fontsize=14)
+
+# Add a legend to each subplot
+axes[0].legend(fontsize=12)
+axes[1].legend(fontsize=12)
+
+# Add gridlines to each subplot
+axes[0].grid(True)
+axes[1].grid(True)
+
+# Display the plot
+plt.tight_layout()
+plt.show()
